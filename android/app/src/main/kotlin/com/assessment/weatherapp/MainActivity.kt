86d1c316f8c2064c7f3f86456abc06dad9915d71
AmalH <hichri.amal@hotmail.fr>
2021-07-05 17:16:08 +0200

    reset repo

--- android/app/src/main/kotlin/com/assessment/weatherapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.assessment.weatherapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/config.dart
+++
@@ -1,3 +0,0 @@
-class Config {
-  static const String apikey = "95b9fafbed459da520ec1a6ba9598c26";
-}
